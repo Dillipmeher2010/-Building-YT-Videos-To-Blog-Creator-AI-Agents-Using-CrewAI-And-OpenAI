9397fcf5ccbdeca228d4937efa84a16af3ddd4ef
DILLIP MEHER2010 <dillipmeher2010@gmail.com>
2024-10-31 10:52:11 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -1,31 +1,30 @@
+# agents.py
 from crewai import Agent
 from tools import tool
 
-
-# Creating a senior researcher agent with memory and verbose mode
+# Researcher agent
 researcher = Agent(
-  role='Blogs Creator from Youtube Videos',
-  goal='provide the relevant video suggestions for the topic {topic}',
-  verbose=True,
-  memory=True,
-  backstory=(
-    "Expert in understanding videos in AI Data Science , MAchine Learning And GEN AI and providing suggestion"
-  ),
-  tools=[tool],
-  allow_delegation=True
+    role='Blogs Creator from Youtube Videos',
+    goal='Provide relevant video suggestions for the topic {topic}',
+    verbose=True,
+    memory=True,
+    backstory=(
+        "Expert in understanding videos in AI, Data Science, Machine Learning, and Gen AI and providing suggestions."
+    ),
+    tools=[tool],
+    allow_delegation=True
 )
 
-# Creating a writer agent with custom tools and delegation capability
+# Writer agent
 writer = Agent(
-  role='Writer',
-  goal='Narrate compelling tech stories about the video {topic}',
-  verbose=True,
-  memory=True,
-  backstory=(
-    "With a flair for simplifying complex topics, you craft"
-    "engaging narratives that captivate and educate, bringing new"
-    "discoveries to light in an accessible manner."
-  ),
-  tools=[tool],
-  allow_delegation=False
+    role='Writer',
+    goal='Narrate compelling tech stories about the video {topic}',
+    verbose=True,
+    memory=True,
+    backstory=(
+        "With a flair for simplifying complex topics, you craft engaging narratives that captivate and educate, "
+        "bringing new discoveries to light in an accessible manner."
+    ),
+    tools=[tool],
+    allow_delegation=False
 )
