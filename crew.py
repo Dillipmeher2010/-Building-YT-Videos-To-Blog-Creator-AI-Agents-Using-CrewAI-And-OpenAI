a8feaf9aaada1950abcc062ac83699f74e4bab47
DILLIP MEHER2010 <dillipmeher2010@gmail.com>
2024-10-31 11:29:52 +0530

    Update crew.py

--- crew.py
+++ crew.py
@@ -1,19 +1,28 @@
 # crew.py
+import streamlit as st
 from crewai import Crew, Process
 from agents import researcher, writer
 from task import research_task, write_task
 
-# Forming the tech-focused crew with caching enabled
-crew = Crew(
-    agents=[researcher, writer],
-    tasks=[research_task, write_task],
-    process=Process.sequential,
-    memory=True,
-    cache=True,
-    max_rpm=100,
-    share_crew=True
-)
+def main():
+    st.title("YouTube Video to Blog Creator")
 
-# This function will be called in the Streamlit app
-def kickoff_crew(topic):
-    return crew.kickoff(inputs={'topic': topic})
+    # Starting the task execution process
+    try:
+        result = Crew(
+            agents=[researcher, writer],
+            tasks=[research_task, write_task],
+            process=Process.sequential,
+            memory=True,
+            cache=True,
+            max_rpm=100,
+            share_crew=True
+        ).kickoff(inputs={'topic': 'AI vs ML VS DL VS Data Science'})
+        
+        st.write(result)
+
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
